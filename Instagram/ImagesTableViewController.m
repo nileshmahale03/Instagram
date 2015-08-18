ecd8b051f6f9e1563261e2f289418176fd20dc2f
nileshmahale <nileshmahale03@gmail.com>
2015-08-17 21:55:50 -0700

    Assignment 27 Completed

--- Instagram/ImagesTableViewController.m
+++ Instagram/ImagesTableViewController.m
@@ -97,15 +97,15 @@
     return (CGRectGetWidth(self.view.frame)/ image.size.width) * image.size.height;
 }
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
+
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -116,7 +116,7 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
+
 
 /*
 // Override to support rearranging the table view.
