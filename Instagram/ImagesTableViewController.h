e9ee00fd9e900461e724c6ac6640305480e4fc0f
nileshmahale <nileshmahale03@gmail.com>
2015-08-17 21:49:42 -0700

    Checkpoint 27 Completed

---
+++ Instagram/ImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ImagesTableViewController.h
+//  Instagram
+//
+//  Created by Nilesh Mahale on 8/17/15.
+//  Copyright (c) 2015 Nilesh Mahale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
---
+++ Instagram/Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Nilesh Mahale on 8/17/15.
+//  Copyright (c) 2015 Nilesh Mahale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
