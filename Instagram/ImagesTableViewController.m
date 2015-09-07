5553328803a6a64cece04061fdb867d9b44e8f6d
nileshmahale <nileshmahale03@gmail.com>
2015-09-06 20:37:08 -0700

    Checkpoint 39 completed

--- Instagram/ImagesTableViewController.m
+++ Instagram/ImagesTableViewController.m
@@ -194,6 +194,18 @@
     }
 }
 
+- (void) cellDidPressLikeButton:(MediaTableViewCell *)cell {
+    Media *item = cell.mediaItem;
+    
+    [[DataSource sharedInstance] toggleLikeOnMediaItem:item withCompletionHandler:^{
+        if (cell.mediaItem == item) {
+            cell.mediaItem = item;
+        }
+    }];
+    
+    cell.mediaItem = item;
+}
+
 @end
 
 
---
+++ Instagram/Instagram/CircleSpinnerView.h
@@ -0,0 +1,17 @@
+//
+//  CircleSpinnerView.h
+//  Instagram
+//
+//  Created by Nilesh Mahale on 9/6/15.
+//  Copyright (c) 2015 Nilesh Mahale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleSpinnerView : UIView
+
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, strong) UIColor *strokeColor;
+
+@end
